399a827687cf72cb0fcdd2b242faf4c048f014bf
unknown <estuam@SALAF402-23.UAM.LOCAL>
2017-02-25 09:38:15 -0500

    Se crearon vistas para el controlador del GIT

--- app/Http/Controllers/sitioController.php
+++ app/Http/Controllers/sitioController.php
@@ -7,11 +7,11 @@ use Illuminate\Http\Request;
 class sitioController extends Controller
 {
     public function index(){
-        echo "Hola Mundo" ;
+        return view ('sitio/index');
     }
 
     public function saludar(Request $request, $nombre){
-        echo "Hola ". $nombre;
+       return view ('sitio/saludar', ['elnombre'=>$nombre]);
     }
     //
 }
---
+++ resources/views/sitio/saludar.blade.php
@@ -0,0 +1,1 @@
+Hola {!! $elnombre !!} desde una vista
